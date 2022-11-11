69e35dfac289fe4a80ec246da48c2d4fbb68ea60
Valdemar Palmquist <valpa404@student.liu.se>
2022-11-11 14:11:21 +0000

    new collisions and box types

--- gameobjects.py
+++ gameobjects.py
@@ -255,6 +255,14 @@ class Box(GamePhysicsObject):
         """ It takes as arguments the coordinate of the starting position of the box (x,y) and the box model (boxmodel). """
         super().__init__(x, y, 0, sprite, space, movable)
         self.destructable = destructable
+            #Woodbox = 3, Rockbox = 4 and metalbox = 5
+        if sprite == images.woodbox:
+            self.shape.collision_type = 3
+        if sprite == images.rockbox:
+            self.shape.collision_type = 4
+        if sprite == images.metalbox:
+            self.shape.collision_type = 5    
+
 
 def get_box_with_type(x, y, type, space):
     (x, y) = (x + 0.5, y + 0.5) # Offsets the coordinate to the center of the tile
