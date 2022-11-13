720575c7e50fb3c05979ec34652b8fb5112a971f
danal315 <danal315@student.liu.se>
2022-11-13 23:54:55 +0100

    cleaned the code

--- ctf.py
+++ ctf.py
@@ -43,7 +43,7 @@ screen = pygame.display.set_mode(current_map.rect().size)
 
 #-- Generate the background
 background = pygame.Surface(screen.get_size())
-
+#   Copy the grass tile all over the level area
 def create_grass():
     for x in range(0, current_map.width):
         for y in range(0,  current_map.height):
@@ -51,9 +51,10 @@ def create_grass():
             # contained in "images.grass" into the "background"
             # image at the coordinates given as the second argument
             background.blit(images.grass,  (x*images.TILE_SIZE, y*images.TILE_SIZE))
-#   Copy the grass tile all over the level area
 
 
+
+#-- Create the boxes
 def create_boxes():
     for x in range(0, current_map.width):
         for y in range(0,  current_map.height):
@@ -65,9 +66,8 @@ def create_boxes():
                 # and the pymunk space
                 box = gameobjects.get_box_with_type(x, y, box_type, space)
                 game_objects_list.append(box)
-#-- Create the boxes
 
-
+#-- Create the tanks
 def create_tanks():
     # Loop over the starting poistion
     for i in range(0, len(current_map.start_positions)):
@@ -79,16 +79,14 @@ def create_tanks():
         tanks_list.append(tank)
         game_objects_list.append(tank)
 
-
-#-- Create the tanks
+#-- Create the bases
 def create_bases():
     for i in range(0,len(current_map.start_positions)):   
         pos_base = current_map.start_positions[i]
         bases = gameobjects.GameVisibleObject(pos_base[0], pos_base[1], images.bases[i])
         game_objects_list.append(bases)
- #-- Create the bases
 
-
+#-- Create out of bound walls
 def create_out_of_bounds():
     body = pymunk.Body(body_type=pymunk.Body.STATIC)
     walls  = [pymunk.Segment(body,(0,0),(0, current_map.width),0),
@@ -96,7 +94,6 @@ def create_out_of_bounds():
                 pymunk.Segment(body,(0, current_map.width), (current_map.height, current_map.width), 0),
                 pymunk.Segment(body,(current_map.height, 0), (current_map.height, current_map.width), 0)]
     space.add(walls)
-#-- Create out of bound walls
 
 
 #<INSERT CREATE FLAG>
@@ -106,22 +103,113 @@ def create_flag():
     return flag
 
 #Creating all the objects
-create_grass()
-create_boxes()
-create_tanks()
-create_bases()
-create_out_of_bounds()
-flag = create_flag()
+
 
 #----- Main Loop -----#
+def main_loop():
+    running = True 
+    skip_update = 0
+    
+    while running:
+        #-- Handle the events
+        for event in pygame.event.get():
+            # Check if we receive a QUIT event (for instance, if the user press the
+            # close button of the wiendow) or if the user press the escape key.
+            
+            if event.type == QUIT:
+                running = False
+            
+            if event.type ==  KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
 
-#-- Control whether the game run
-running = True
+                elif event.key == K_UP:
+                    tanks_list[player_tank].accelerate() 
+                    for tanks in tanks_list:
+                        tanks.accelerate()
 
-skip_update = 0
+                elif event.key == K_DOWN:
+                    tanks_list[player_tank].decelerate()
+
+                elif event.key == K_LEFT:
+                    tanks_list[player_tank].turn_left()
+
+                elif event.key == K_RIGHT:
+                    tanks_list[player_tank].turn_right()
+                elif event.key == K_SPACE:
+                    bullet_list.append(tanks_list[player_tank].shoot(space))
+                    
+                
+
+            if event.type == KEYUP:
+                if event.key == K_UP:
+                    tanks_list[player_tank].stop_moving() 
+                    tanks_list[player_tank].stop_turning()
+
+                elif event.key == K_DOWN:
+                    tanks_list[player_tank].stop_moving() 
+                    tanks_list[player_tank].stop_turning()
+
+                elif event.key == K_LEFT:
+                    tanks_list[player_tank].stop_turning()
+
+                elif event.key == K_RIGHT:
+                    tanks_list[player_tank].stop_turning()    
+
+        #-- Update physics
+        if skip_update == 0:
+            #  Loop over all the game objects and update their speed in function of their
+            # acceleration.
+            for obj in game_objects_list:
+                obj.update()
+            skip_update = 5
+        else:
+            skip_update -= 1
+
+        #   Check collisions and update the objects position
+        space.step(1 / FRAMERATE)
+
+        #   Update object that depends on an other object position (for instance a flag)
+        for obj in game_objects_list:
+            obj.post_update()
+
+        #-- Update Display
+        # Display the background on the screen
+        screen.blit(background, (0, 0))
+
+        # Update the display of the game objects on the screen
+        for obj in game_objects_list:
+            obj.update_screen(screen)
+        # Update to display bullets
+        for bullet in bullet_list:
+            bullet.update_screen(screen)
+        #Tank update
+        for tank in tanks_list:
+            tank.update_screen(screen)
+        
+        #Base update
+        for bases in bases_list:
+            bases.update_screen(screen)
+
+        #Flag update
+        for tanks in tanks_list:
+            tanks.try_grab_flag(flag)
+            
+        for tank in tanks_list:
+            if tank.has_won() == True:
+                running = False
+        flag.update_screen(screen)
+        #Tank has won - only works with player tank
+
+
+
+        #   Redisplay the entire screen (see double buffer technique)
+        pygame.display.flip()
+
+        #   Control the game framerate
+        clock.tick(FRAMERATE)
 
 #-- Create collision handlers 
-   
 
 def collision_bullet_woodbox(arb,space,data):
     """TODO make the sprite disappear"""
@@ -195,102 +283,13 @@ handler.pre_solve = collision_bullet_tank
     
 player_tank = 0 #Index of whcih tank the player controlls 
 
-while running:
-    #-- Handle the events
-    for event in pygame.event.get():
-        # Check if we receive a QUIT event (for instance, if the user press the
-        # close button of the wiendow) or if the user press the escape key.
-        
-        if event.type == QUIT:
-            running = False
-        
-        if event.type ==  KEYDOWN:
-            if event.key == K_ESCAPE:
-                running = False
-
-            elif event.key == K_UP:
-                tanks_list[player_tank].accelerate() 
-
-            elif event.key == K_DOWN:
-                tanks_list[player_tank].decelerate()
-
-            elif event.key == K_LEFT:
-                tanks_list[player_tank].turn_left()
-
-            elif event.key == K_RIGHT:
-                tanks_list[player_tank].turn_right()
-            elif event.key == K_SPACE:
-                bullet_list.append(tanks_list[player_tank].shoot(space))
-                
-            
-
-        if event.type == KEYUP:
-            if event.key == K_UP:
-                tanks_list[player_tank].stop_moving() 
-                tanks_list[player_tank].stop_turning()
-
-            elif event.key == K_DOWN:
-                tanks_list[player_tank].stop_moving() 
-                tanks_list[player_tank].stop_turning()
-
-            elif event.key == K_LEFT:
-                tanks_list[player_tank].stop_turning()
-
-            elif event.key == K_RIGHT:
-                tanks_list[player_tank].stop_turning()    
-
-    #-- Update physics
-    if skip_update == 0:
-        #  Loop over all the game objects and update their speed in function of their
-        # acceleration.
-        for obj in game_objects_list:
-            obj.update()
-        skip_update = 5
-    else:
-        skip_update -= 1
-
-    #   Check collisions and update the objects position
-    space.step(1 / FRAMERATE)
-
-    #   Update object that depends on an other object position (for instance a flag)
-    for obj in game_objects_list:
-        obj.post_update()
-
-    #-- Update Display
-    # Display the background on the screen
-    screen.blit(background, (0, 0))
-
-    # Update the display of the game objects on the screen
-    for obj in game_objects_list:
-        obj.update_screen(screen)
-    # Update to display bullets
-    for bullet in bullet_list:
-        bullet.update_screen(screen)
-    #Tank update
-    for tank in tanks_list:
-        tank.update_screen(screen)
-    
-    #Base update
-    for bases in bases_list:
-        bases.update_screen(screen)
-
-    #Flag update
-    for tanks in tanks_list:
-        tanks.try_grab_flag(flag)
-        
-    for tank in tanks_list:
-        if tank.has_won() == True:
-            running = False
-    flag.update_screen(screen)
-    #Tank has won - only works with player tank
-
-
-
-    #   Redisplay the entire screen (see double buffer technique)
-    pygame.display.flip()
-
-    #   Control the game framerate
-    clock.tick(FRAMERATE)
+create_grass()
+create_boxes()
+create_tanks()
+create_bases()
+create_out_of_bounds()
+flag = create_flag()
+main_loop()
 
 
 
